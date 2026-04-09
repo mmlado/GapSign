c25452d4d301baf3395e46203a59c146d34602cf
Mladen Milankovic <developer@mmlado.com>
2026-04-09 15:46:28 +0200

    chore: fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -9,4 +9,20 @@ module.exports = [
   {
     ignores: ['node_modules/**', 'android/**', 'ios/**'],
   },
+  {
+    files: ['**/__tests__/**', '**/*.test.{ts,tsx}', 'jest.setup.js'],
+    languageOptions: {
+      globals: {
+        jest: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+      },
+    },
+  },
 ];
